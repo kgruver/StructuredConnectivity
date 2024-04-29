87519bb67fd8f14c69e67878c23c301cd2496b4d
kgruver <55257289+kgruver@users.noreply.github.com>
2024-04-29 08:17:48 -0700

    Update Unsupervised_hierarchical_clustering_Fig2.py

--- Datasets and code/Unsupervised clustering Fig. 2/Unsupervised_hierarchical_clustering_Fig2.py
+++ Datasets and code/Unsupervised clustering Fig. 2/Unsupervised_hierarchical_clustering_Fig2.py
@@ -8,7 +8,7 @@ cmap2 = matplotlib.colors.LinearSegmentedColormap.from_list("", ["darkgrey","cri
 
 cell_connected_to_lobules_demo = pd.read_csv ('Cell_connected_to_lobules_boolean_demo_10cells.csv')
 
-sn.clustermap(cell_connected_to_lobules_demo,figsize=(6,6), method='average', annot_kws={"fontsize":16}, metric='hamming', col_cluster=True, row_cluster=False, annot=False, cmap=cmap2)
+sn.clustermap(cell_connected_to_lobules,figsize=(6,6), tree_kws=dict(linewidths=3, color="black"), method='average', annot_kws={"fontsize":16}, metric='hamming', col_cluster=True, row_cluster=False, annot=False, linewidths=1, cmap=cmap2)
 plt.show()
 
 
